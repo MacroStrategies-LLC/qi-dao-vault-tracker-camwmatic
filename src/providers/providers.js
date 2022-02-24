f2cf3f5d3c573abb24a166a3b5cfd3998d6569d6
jalamis <jalamis@remax.com>
2022-02-24 02:10:46 -0600

    remove comments from providers

--- src/providers/providers.js
+++ src/providers/providers.js
@@ -1,9 +1,5 @@
-/**
- * @module provider
- */
 import {ethers} from 'ethers';
 const infura = new ethers.providers.JsonRpcProvider(process.env.infura);
-// const alchemy = new ethers.providers.JsonRpcProvider(process.env.alchemy);
 const alchemy = new ethers.providers.AlchemyProvider(
     'matic', process.env.alchemy);
 const providers = {
