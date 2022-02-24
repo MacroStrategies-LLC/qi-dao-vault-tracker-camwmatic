7966b6354652cf0719fbf74d2cfb9c1b4de487f8
jalamis <jalamis@remax.com>
2022-02-24 02:12:52 -0600

    remove jsdoc from wallet, leave in case we need for funcs

--- src/wallet/wallet.js
+++ src/wallet/wallet.js
@@ -1,6 +1,3 @@
-/**
- * @module wallet
- */
 import {ethers} from 'ethers';
 import providers from '#src/providers/providers.js';
 const wallet = new ethers.Wallet(process.env.private_key, providers.alchemy);
